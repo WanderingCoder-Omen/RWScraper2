5de00e262c5b2fbd690726038e55b8757ff32579
WanderingCoder-Omen <devops.omen@gmail.com>
2023-10-03 07:31:41 +0000

    time change

--- runner.py
+++ runner.py
@@ -26,14 +26,14 @@ def run_daily():
     print("All scrapers have finished.")
 
 # Schedule the script to run daily at a specific time (e.g., 1:00 AM)
-schedule.every().day.at("10:55").do(run_daily)
+schedule.every().day.at("07:32").do(run_daily)
 
 while True:
     schedule.run_pending()
     
     # Check if it's 1:00 AM and scrapers haven't finished
     current_time = time.strftime("%H:%M")
-    if current_time == "07:30":
-        print("Scrapers haven't finished by 11:00 AM. Waiting for 2 hours...")
+    if current_time == "07:35":
+        print("Scrapers haven't finished by 07:35 AM. Waiting for 2 hours...")
         time.sleep(2 * 60 * 60)  # Wait for 2 hours (in seconds) = 2 * 60 * 60 seconds = 7200 seconds
         # After 2 hours, the script will continue to the next scheduling iteration (3:00 AM).
